8f182c388331ec6afed3ea8dbe03a8f2ef1db61a
Huzaifa <huzaifakhanned@gmail.com>
2023-07-17 03:12:19 +0500

    Done

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-export type Note = {
-  id : string,
-  title : string,
-  markdown : string,
-  tags : Tag[]
-}
-
-export type NoteData = {
-  title : string,
-  markdown : string,
-  tags : Tag[]
-}
-
-export type Tag = {
-  id : string,
-  name : string
-}
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
